f73b286ac9dffff0c35a369030eec36e0e414b53
tomelsom <te28@hotmail.com>
2025-05-09 17:35:12 +0100

    added nav

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <div class="mainbackground">
-      <h1 class="redtext container">Hello World</h1>
+      <h1 class="redtext container">Welcome to Level X</h1>
       <p class="colorred container">lets see if this works</p>
     </div>
 </x-layout>
